d5e7e9950476ef775a4f9e43270a75864e789af9
Neil Holloway <neil_holloway@hotmail.co.uk>
2023-04-16 20:47:33 +0100

    test

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from to_consume.streamlit.login import get_authenticator, get_user_id
 import streamlit as st
 from streamlit_profiler import Profiler
 
+from to_consume.utils import db_conn
+
 # TODO seasons
 # TODO dedicated watchlist table
 # TODO refresh imdb ratings
@@ -15,16 +17,21 @@ st.set_page_config(
     layout="wide",
 )
 
-authenticator = get_authenticator()
-name, authentication_status, username = authenticator.login("Login", "main")
+# authenticator = get_authenticator()
+# name, authentication_status, username = authenticator.login("Login", "main")
 
-if authentication_status:
-    authenticator.logout("Logout", "main")
-    st.write(f"Welcome *{name}*")
-    st.session_state.user_id = get_user_id(username)
-    with Profiler():
-        main_app()
-elif authentication_status is False:
-    st.error("Username/password is incorrect")
-elif authentication_status is None:
-    st.warning("Please enter your username and password")
+# if authentication_status:
+#     authenticator.logout("Logout", "main")
+#     st.write(f"Welcome *{name}*")
+#     st.session_state.user_id = get_user_id(username)
+#     with Profiler():
+#         main_app()
+# elif authentication_status is False:
+#     st.error("Username/password is incorrect")
+# elif authentication_status is None:
+#     st.warning("Please enter your username and password")
+with Profiler():
+    conn = db_conn()
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT * FROM cache")
+        res = cursor.fetchall()
